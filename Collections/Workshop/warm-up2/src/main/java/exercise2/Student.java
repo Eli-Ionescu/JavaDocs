f7402de3deb58a0b1c67d45febb0303c87f0a34f
Eli Ionescu <ionescuelena95@gmail.com>
2016-07-05 11:16:06 +0300

    Maven

---
+++ Collections/Workshop/warm-up2/src/main/java/exercise2/Student.java
@@ -0,0 +1,86 @@
+package exercise2;
+
+/**
+ * Created by Radu.Hoaghe on 20.04.2015.
+ */
+public class Student {
+    private final Integer id;
+    private final String firstName;
+    private final String lastName;
+    private final Double averageGrade;
+
+    public Student(Integer id, String firstName, String lastName, Double averageGrade) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.averageGrade = averageGrade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Double getAverageGrade() {
+        return averageGrade;
+    }
+
+    // TODO Exercise 2 a) Override the toString() method
+    @Override
+    public String toString() {
+        return "ID: "+id + "\nFirst Name: "+ firstName +"\nLast Name: "+
+                lastName + "\nAverage Grade: "+averageGrade;
+    }
+
+    // TODO Exercise 2 c) Override the equals() method
+    @Override
+    public boolean equals(Object o) {
+        // TODO Exercise 2 c1) Check if the current instance is the same instance as the one from Object o
+        if(this == o) return true;
+
+        // TODO Exercise 2 c2) Check if Object o is null
+
+        // TODO Exercise 2 c3) Check if Object o class type is the same as the current instance's type
+        if(o == null || !this.getClass().equals(o.getClass())) return false;
+
+        // TODO Exercise 2 c4) Now you know for sure that the Object o is of type Student so you
+        // TODO need to cast it to a Student type object
+
+        Student obj = (Student)o;
+
+        // TODO Exercise 2 c5) Check if all the fields from Student class are equal to the ones from
+        // TODO Exercise 2 c5) Object o (id, lastName, firstName, averageGrade)
+        if(obj.id.equals(id) &&
+                obj.firstName.equals(firstName) &&
+                obj.lastName.equals(lastName) &&
+                obj.averageGrade.equals(averageGrade))
+            return true;
+
+        return false;
+
+        // TODO Exercise 2 d) After you finished implementing equals method go to TODO Exercise 2 e) from Exercise2 class
+    }
+
+    // TODO Exercise 2 g) Override the hashCode() method
+    // TODO Exercise 2 g) Hint: Don't forget to include in the hashCode result all the fields from
+    // TODO Exercise 2 g) the Student class
+    @Override
+    public int hashCode() {
+        int result = 17;
+        // For the id :
+        result = 31 * result + id.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + averageGrade.hashCode();
+        return result;
+        // TODO Exercise 2 h) After you finished implementing hashCode go to TODO Exercise 2 i) from Exercise2 class
+    }
+
+}
